9a405ae0661f423642c2360de3c1003b5bea61b1
Ain-Jeong <ain.jeong@stu.ocsb.ca>
2025-03-28 17:17:08 +0000

    init

--- js/script.js
+++ js/script.js
@@ -1,5 +1,31 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2025 Ain Jeong All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Ain Jeong
+// Created on: Mar 2025
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * This function calculates the volume of a pyramid.
+ */
+function calculateVolumeOfPyramid() {
+  // input
+  const lengthOfPyramid = parseFloat(
+    document.getElementById("length-of-pyramid").value
+  )
+  const widthOfPyramid = parseFloat(
+    document.getElementById("width-of-pyramid").value
+  )
+  const heightOfPyramid = parseFloat(
+    document.getElementById("height-of-pyramid").value
+  )
+
+  // process
+  const volumeOfPyramid =
+    (lengthOfPyramid * widthOfPyramid * heightOfPyramid) / 3
+
+  // output
+  document.getElementById("answer").innerHTML =
+    "Volume of your Pyramid is: " + volumeOfPyramid + " mm²"
+}
